8cd2afe33481457bac079d4ea961879e14b22c0d
KIvan <kIvan.by@yandex.by>
2021-04-18 21:22:57 +0300

    Изменил структуру VideoGluer_Preview.py

--- VideoGluer_Preview.py
+++ VideoGluer_Preview.py
@@ -1,8 +1,10 @@
 from moviepy.editor import VideoFileClip, clips_array
-
-game = VideoFileClip("myvideo.mp4")  # add 10px contour
-statistic = VideoFileClip("myvideo1.mp4")
-final_clip = clips_array([[game, statistic]])
-final_clip.audio = final_clip.audio.set_fps(game.audio.fps)  # без нее ничего не работает хз почему
-final_clip.preview(fps=game.fps)
-final_clip.write_videofile("my_stack.mp4", threads=8) # нужно понять что значат эти потоки, а также желательно подключиьт многопоточность и асинхронность
+def preview():
+    game = VideoFileClip("myvideo.mp4")  # add 10px contour
+    statistic = VideoFileClip("myvideo1.mp4")
+    final_clip = clips_array([[game, statistic]])
+    final_clip.audio = final_clip.audio.set_fps(game.audio.fps)  # без нее ничего не работает хз почему
+    final_clip.preview(fps=game.fps)
+    final_clip.write_videofile("my_stack.mp4", threads=8) # нужно понять что значат эти потоки, а также желательно подключиьт многопоточность и асинхронность
+if __name__ == '__main__':
+    preview()
