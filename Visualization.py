a82cedbaa5da834f6fc7dab22d343caae48e5439
KIvan <kIvan.by@yandex.by>
2021-04-18 14:39:21 +0300

    Переписал  Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -3,31 +3,33 @@ import matplotlib.pyplot as plt
 import csv
 from pylab import rcParams
 
-rcParams['figure.figsize'] = 16, 5
+def BuilderAnalytics(FILENAME):
+    rcParams['figure.figsize'] = 16, 5 # редактирование размера изображения в дюймах, нужно как-то автоматом делать
 
-FILENAME = 'Head.csv'
+    Number_OF_Keystrokes = {}
 
-Number_OF_Keystrokes = {}
+    with open(FILENAME, "r", newline="") as file:
+        reader = csv.reader(file)
+        s = 0
+        for row in reader:
 
-with open(FILENAME, "r", newline="") as file:
-    reader = csv.reader(file)
-    s = 0
-    for row in reader:
+            if len(row[1]) > 3:
+                a = row[1][1:-1].split(',')
+                for element in a:
 
-        if len(row[1]) > 3:
-            a = row[1][1:-1].split(',')
-            for element in a:
+                    if element not in Number_OF_Keystrokes:
+                        Number_OF_Keystrokes[element] = 1
+                    else:
+                        Number_OF_Keystrokes[element] += 1
+    #print(Number_OF_Keystrokes)
 
-                if element not in Number_OF_Keystrokes:
-                    Number_OF_Keystrokes[element] = 1
-                else:
-                    Number_OF_Keystrokes[element] += 1
-print(Number_OF_Keystrokes)
+    Number_OF_Keystrokes = pd.Series(Number_OF_Keystrokes)
+    plt.title("Количество нажатий клавиш")
+    plt.bar(Number_OF_Keystrokes.index, height=Number_OF_Keystrokes)
+    plt_output = plt.gcf()
+    plt.show() # нужно будет убрать
+    plt.draw() # нужно будет убрать
+    plt_output.savefig('NumberOfKeystrokes.png', dpi=100)
 
-Number_OF_Keystrokes = pd.Series(Number_OF_Keystrokes)
-plt.title("Количество нажатий клавиш")
-plt.bar(Number_OF_Keystrokes.index, height=Number_OF_Keystrokes)
-plt_output = plt.gcf()
-plt.show()
-plt.draw()
-plt_output.savefig('NumberOfKeystrokes.png', dpi=100)
+if __name__ == '__main__':
+    BuilderAnalytics('Head.csv')
