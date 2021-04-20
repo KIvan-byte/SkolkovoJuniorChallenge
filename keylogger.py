574b8e366309e3bbc17734a4a848464ec2d82f70
KIvan <kIvan.by@yandex.by>
2021-04-20 08:13:41 +0300

    Версия\  МАКСА

---
+++ keylogger.py
@@ -0,0 +1,42 @@
+import pynput.keyboard as Keyboard
+import csv
+import datetime
+
+
+stop_tracker = None
+
+
+def writeToCSV(fields):
+	with open(r'C:\Users\Maxim\Desktop\Skolkovo\keys.csv', 'a', newline='') as f:
+		writer = csv.writer(f)
+		writer.writerow(fields)
+
+
+def on_press(key):
+	# Callback function whenever a key is pressed
+	now = datetime.datetime.now()
+	try:
+		writeToCSV([key.char, str(now.isoformat())])
+	except AttributeError:
+		writeToCSV([key, str(now.isoformat())])
+
+
+def start_keylogger():
+	global stop_tracker
+	with Keyboard.Listener(on_press=on_press) as listener:
+		print('enter')
+		stop_tracker = listener
+		listener.join()
+		print('end')
+	print('end th')
+
+
+def stop():
+	global stop_tracker
+	if stop_tracker != None:
+		stop_tracker.stop()
+		stop_tracker = None
+	print(stop_tracker)
+
+
+
---
+++ test2.py
@@ -0,0 +1,35 @@
+import cv2
+import pyautogui
+import numpy as np
+
+p = pyautogui.size()
+img1 = cv2.imread('ex1.jpg')
+img2 = cv2.imread('hms_victory.jpg')
+
+def hconcat_resize(img_list,
+                   interpolation
+                   =cv2.INTER_CUBIC):
+    # take minimum hights
+    h_min = min(img.shape[0]
+                for img in img_list)
+
+    # image resizing
+    im_list_resize = [cv2.resize(img,
+                                 (int(img.shape[1] * h_min / img.shape[0]),
+                                  h_min), interpolation
+                                 =interpolation)
+                      for img in img_list]
+
+    # return final image
+    return cv2.hconcat(im_list_resize)
+
+
+
+
+def makeOne(img):
+    img_h_resize = hconcat_resize([cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB), img2])
+    return cv2.resize(img_h_resize, p)
+    # show the Output image
+# cv2.imshow('hconcat_resize.jpg', makeOne(pyautogui.screenshot()))
+# cv2.waitKey(0)
+
